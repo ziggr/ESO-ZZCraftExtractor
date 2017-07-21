31bbc32c1c3cb9d8c7cfd2ac481bbd5a465a870e
Zig Zichterman <ziggr@ziggr.com>
2017-07-20 19:47:50 -0700

    Good table data
    Instead of 200 lines of scroll blindness, compact the data into one tab-delimited line per item (1h axe, 1h mace, cuirass, others) with each of that item's different traits listed. Data is much easier for this human to read.
    
    Data file here has some holes in Clever Alchemist weapons due to a mat_ct bug that existed in previous commit. Fixed in code now, but I have not yet had a chance to update the Clever Alchemist data in this file.

--- ZZCraftExtractor.lua
+++ ZZCraftExtractor.lua
@@ -40,52 +40,52 @@ local ARMOR_TRAITS    = {
 local STATION_WORK = {
   [CRAFTING_TYPE_BLACKSMITHING] = {
     station_work = {
-                     {  1, WEAPON_TRAITS } -- 1h axe
-                   , {  2, WEAPON_TRAITS } -- 1h mace
-                   , {  3, WEAPON_TRAITS } -- 1h sword
-                   , {  4, WEAPON_TRAITS } -- 2h battleaxe
-                   , {  6, WEAPON_TRAITS } -- 2h greatsword
-                   , {  5, WEAPON_TRAITS } -- 2h maul
-                   , {  7, WEAPON_TRAITS } -- dagger
-                   , {  8, ARMOR_TRAITS  } -- heavy cuirass
-                   , {  9, ARMOR_TRAITS  } -- heavy sabatons
-                   , { 10, ARMOR_TRAITS  } -- heavy gauntlets
-                   , { 11, ARMOR_TRAITS  } -- heavy hHelm
-                   , { 12, ARMOR_TRAITS  } -- heavy greaves
-                   , { 13, ARMOR_TRAITS  } -- heavy pauldron
-                   , { 14, ARMOR_TRAITS  } -- heavy girdle
+                     { pattern_index =  1, mat_ct = 3, traits = WEAPON_TRAITS } -- 1h axe
+                   , { pattern_index =  2, mat_ct = 3, traits = WEAPON_TRAITS } -- 1h mace
+                   , { pattern_index =  3, mat_ct = 3, traits = WEAPON_TRAITS } -- 1h sword
+                   , { pattern_index =  4, mat_ct = 5, traits = WEAPON_TRAITS } -- 2h battleaxe
+                   , { pattern_index =  6, mat_ct = 5, traits = WEAPON_TRAITS } -- 2h greatsword
+                   , { pattern_index =  5, mat_ct = 5, traits = WEAPON_TRAITS } -- 2h maul
+                   , { pattern_index =  7, mat_ct = 2, traits = WEAPON_TRAITS } -- dagger
+                   , { pattern_index =  8, mat_ct = 7, traits = ARMOR_TRAITS  } -- heavy cuirass
+                   , { pattern_index =  9, mat_ct = 5, traits = ARMOR_TRAITS  } -- heavy sabatons
+                   , { pattern_index = 10, mat_ct = 5, traits = ARMOR_TRAITS  } -- heavy gauntlets
+                   , { pattern_index = 11, mat_ct = 5, traits = ARMOR_TRAITS  } -- heavy helm
+                   , { pattern_index = 12, mat_ct = 6, traits = ARMOR_TRAITS  } -- heavy greaves
+                   , { pattern_index = 13, mat_ct = 5, traits = ARMOR_TRAITS  } -- heavy pauldron
+                   , { pattern_index = 14, mat_ct = 5, traits = ARMOR_TRAITS  } -- heavy girdle
                    }
 , set_bonus_pattern_offset = 14
 }
 , [CRAFTING_TYPE_CLOTHIER     ] = {
     station_work = {
-                     {  1, ARMOR_TRAITS  } -- light robe
-                   , {  2, ARMOR_TRAITS  } -- light jerkin
-                   , {  3, ARMOR_TRAITS  } -- light shoes
-                   , {  4, ARMOR_TRAITS  } -- light gloves
-                   , {  5, ARMOR_TRAITS  } -- light hat
-                   , {  6, ARMOR_TRAITS  } -- light breeches
-                   , {  7, ARMOR_TRAITS  } -- light epaulets
-                   , {  8, ARMOR_TRAITS  } -- light sash
-                   , {  9, ARMOR_TRAITS  } -- medium jack
-                   , { 10, ARMOR_TRAITS  } -- medium boots
-                   , { 11, ARMOR_TRAITS  } -- medium bracers
-                   , { 12, ARMOR_TRAITS  } -- medium helmet
-                   , { 13, ARMOR_TRAITS  } -- medium guards
-                   , { 14, ARMOR_TRAITS  } -- medium arm cops
-                   , { 15, ARMOR_TRAITS  } -- medium belt
+                     { pattern_index =  1, mat_ct = 7, traits = ARMOR_TRAITS  } -- light robe
+                   , { pattern_index =  2, mat_ct = 7, traits = ARMOR_TRAITS  } -- light jerkin
+                   , { pattern_index =  3, mat_ct = 5, traits = ARMOR_TRAITS  } -- light shoes
+                   , { pattern_index =  4, mat_ct = 5, traits = ARMOR_TRAITS  } -- light gloves
+                   , { pattern_index =  5, mat_ct = 5, traits = ARMOR_TRAITS  } -- light hat
+                   , { pattern_index =  6, mat_ct = 6, traits = ARMOR_TRAITS  } -- light breeches
+                   , { pattern_index =  7, mat_ct = 5, traits = ARMOR_TRAITS  } -- light epaulets
+                   , { pattern_index =  8, mat_ct = 5, traits = ARMOR_TRAITS  } -- light sash
+                   , { pattern_index =  9, mat_ct = 7, traits = ARMOR_TRAITS  } -- medium jack
+                   , { pattern_index = 10, mat_ct = 5, traits = ARMOR_TRAITS  } -- medium boots
+                   , { pattern_index = 11, mat_ct = 5, traits = ARMOR_TRAITS  } -- medium bracers
+                   , { pattern_index = 12, mat_ct = 5, traits = ARMOR_TRAITS  } -- medium helmet
+                   , { pattern_index = 13, mat_ct = 6, traits = ARMOR_TRAITS  } -- medium guards
+                   , { pattern_index = 14, mat_ct = 5, traits = ARMOR_TRAITS  } -- medium arm cops
+                   , { pattern_index = 15, mat_ct = 5, traits = ARMOR_TRAITS  } -- medium belt
                    }
 , set_bonus_pattern_offset = 15
 }
 
 , [CRAFTING_TYPE_WOODWORKING  ] = {
     station_work = {
-                     {  1, WEAPON_TRAITS } -- bow
-                   , {  3, WEAPON_TRAITS } -- inferno staff
-                   , {  4, WEAPON_TRAITS } -- frost staff
-                   , {  5, WEAPON_TRAITS } -- lightning staff
-                   , {  6, WEAPON_TRAITS } -- healing staff
-                   , {  2, ARMOR_TRAITS  } -- shield
+                     { pattern_index =  1, mat_ct = 3, traits = WEAPON_TRAITS } -- bow
+                   , { pattern_index =  3, mat_ct = 3, traits = WEAPON_TRAITS } -- inferno staff
+                   , { pattern_index =  4, mat_ct = 3, traits = WEAPON_TRAITS } -- frost staff
+                   , { pattern_index =  5, mat_ct = 3, traits = WEAPON_TRAITS } -- lightning staff
+                   , { pattern_index =  6, mat_ct = 3, traits = WEAPON_TRAITS } -- healing staff
+                   , { pattern_index =  2, mat_ct = 6, traits = ARMOR_TRAITS  } -- shield
                    }
 , set_bonus_pattern_offset = 6
 }
@@ -98,7 +98,7 @@ function ZZCraftExtractor.OnStationInteract(event, station)
     local set_index = llc:GetCurrentSetInteractionIndex()
     d("Hello, Station! station:"..tostring(station).." set:"..tostring(set_index))
 
-    local item_id_list = self:BuildItemIdList(station)
+    local item_id_list = self:BuildItemIdList(station, set_index)
     if not item_id_list then
         d("nope")
         return
@@ -109,9 +109,20 @@ function ZZCraftExtractor.OnStationInteract(event, station)
     if not iid[set_index] then iid[set_index] = {} end
     iid[set_index][station] = item_id_list
     d("Done with station:"..tostring(station).." set:"..tostring(set_index) .. " got:"..tostring(#item_id_list))
+
+                        -- Special case for Armor Master: also build
+                        -- the non-set-bonus table when you visit an
+                        -- Armor Master table, just because the BBC crafting
+                        -- boardwalk lacks a plain table.
+    if set_index == 27 then
+        local item_id_list = self:BuildItemIdList(station, 0)
+        if not iid["none"] then iid["none"] = {} end
+        iid["none"][station] = item_id_list
+        d("Done with station:none got:"..tostring(#item_id_list))
+    end
 end
 
-function ZZCraftExtractor:BuildItemIdList(station)
+function ZZCraftExtractor:BuildItemIdList(station, set_index)
     if not STATION_WORK[station] then
         d("no STATION_WORK["..tostring(station).."]")
         return nil
@@ -119,7 +130,10 @@ function ZZCraftExtractor:BuildItemIdList(station)
     local station_work   = STATION_WORK[station].station_work
                         -- A plain "Iron Axe" is pattern_index=1, an "Axe of
                         -- the Armor Master" is pattern_index=1+14=15
-    local pattern_offset = STATION_WORK[station].set_bonus_pattern_offset
+    local pattern_offset = 0
+    if 0 < set_index then
+        pattern_offset = STATION_WORK[station].set_bonus_pattern_offset
+    end
                         -- Traits and motifs must be offset by 1, dunno why.
     local trait_offset   = 1
     local motif_offset   = 1
@@ -128,45 +142,57 @@ function ZZCraftExtractor:BuildItemIdList(station)
     local material_index = 1    -- iron/jute/maple
     local motif_id       = 1     -- Breton
     local link_style    = LINK_STYLE_DEFAULT
-    local list_name     = nil
 
+                        -- For each of the up to 15 different items
+                        -- you can craft at this station, generate a single
+                        -- tab-delimited
+                        -- line with all of that item's itemId numbers,
+                        -- one for each possible trait, starting with no trait.
+                        -- Just to make the file easier to read, also append
+                        -- the item's name.
     for i,sw in ipairs(station_work) do
-        local pattern_index = sw[1]
-        local traits        = sw[2]
-        local material_ct   = 3     -- 3 for level 1 weapons
-        if traits == ARMOR_TRAITS then
-            material_ct = 7         -- 7 for level 1 armor
-        end
-
+        local pattern_index = sw.pattern_index
+        local traits        = sw.traits
+        local mat_ct        = sw.mat_ct
+        local cells = {}
+        local row_name = nil
         for ti, trait_index in ipairs(traits) do
             local item_link = GetSmithingPatternResultLink(
                     pattern_index + pattern_offset
                   , material_index
-                  , material_ct
+                  , mat_ct
                   , motif_id    + motif_offset
                   , trait_index + trait_offset
                   , link_style
                   )
+-- if pattern_index == 4 then
 -- d("GetSmithingPatternResultLink("
 --   ..tostring(pattern_index + pattern_offset)..", "
 --   ..tostring(material_index)..", "
---   ..tostring(material_ct)..", "
---   ..tostring(motif_id + motif_offset)..", "
+--   ..tostring(mat_ct)..", "
+--   ..tostring(motif_id    + motif_offset)..", "
 --   ..tostring(trait_index + trait_offset)..", "
 --   ..tostring(link_style)..
 --     ")")
-d(item_link)
+-- d(item_link)
+-- end
             local item_id = GetItemIDFromLink(item_link)
-            table.insert(item_id_list, item_id)
+            table.insert(cells, item_id)
 
                         -- Insert the first item's name as sort of
                         -- documentation within the saved data.
-            if not list_name then
-                list_name = GetItemLinkName(item_link)
-                item_id_list.name = list_name
-d(list_name)
+            if not row_name then
+                row_name = GetItemLinkName(item_link)
             end
         end
+
+        local fmt_cells = {}
+        for _, c in ipairs(cells) do
+            table.insert(fmt_cells, string.format("%7d", c))
+        end
+        table.insert(fmt_cells, row_name)
+        local row_string = table.concat(fmt_cells, "\t")
+        table.insert(item_id_list, row_string)
     end
     return item_id_list
 end
